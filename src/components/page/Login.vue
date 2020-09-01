c4a6d1b7e67d7ac6965e24ac23075dcf011212e0
kingsun <kingsundad@gmail.com>
2020-09-01 22:57:31 +0800

    修改登录账号密码都为ldmc

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -32,8 +32,8 @@ export default {
     data: function() {
         return {
             param: {
-                username: 'admin',
-                password: '123123',
+                username: 'ldmc',
+                password: 'ldmc',
             },
             rules: {
                 username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
