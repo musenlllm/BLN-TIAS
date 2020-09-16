ca1649de2876fb0a292586d74461f8c33cfb8219
diralpo <295536022@qq.com>
2020-09-16 14:51:37 +0800

    修改主题分类饼图动态展示

--- src/components/page/TopicClassification.vue
+++ src/components/page/TopicClassification.vue
@@ -343,6 +343,31 @@ export default {
       //防止越界，重绘canvas
       window.onresize = myChart.resize;
       myChart.setOption(option);
+
+      var currentIndex = -1;
+
+      setInterval(function () {
+        var dataLen = option.series[0].data.length;
+        // 取消之前高亮的图形
+        myChart.dispatchAction({
+          type: "downplay",
+          seriesIndex: 0,
+          dataIndex: currentIndex,
+        });
+        currentIndex = (currentIndex + 1) % dataLen;
+        // 高亮当前图形
+        myChart.dispatchAction({
+          type: "highlight",
+          seriesIndex: 0,
+          dataIndex: currentIndex,
+        });
+        // 显示 tooltip
+        myChart.dispatchAction({
+          type: "showTip",
+          seriesIndex: 0,
+          dataIndex: currentIndex,
+        });
+      }, 1000);
     },
     onClick(value) {
       //alert("你点击了 " + value);
